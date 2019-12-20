ffa5bcc298ac16ce36201b18406d36fc98b77b14
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-12-20 16:07:05 +0000

    import statement added to app.py file

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import os
 app = Flask(__name__)
 
 # app.config['SECRET_KEY'] = config.SECRET_KEY
-portfolio_username = config.portfolio_username
-portfolio_password = config.portfolio_password
+# portfolio_username = config.portfolio_username
+# portfolio_password = config.portfolio_password
+env_var = os.environ.get('portfolio_username')
+env_var = os.environ.get('portfolio_password')
 
 # Route for home page
 @app.route('/')
